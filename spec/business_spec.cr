56a48c18bd5b550e61c461a87206fae42af5e469
Davide Paolo Tua <davide.paolo.tua@usi.ch>
2020-11-17 19:15:28 +0100

    Fix business card failing spec

--- spec/business_spec.cr
+++ spec/business_spec.cr
@@ -4,7 +4,7 @@ describe Faker::Business do
   it "should return deterministic results when seeded" do
     Faker.seed 123456
     Faker::Business.credit_card_number.should eq "1228-1221-1221-1431"
-    Faker::Business.credit_card_expiry_date.should eq Time.new(2013, 9, 12)
+    Faker::Business.credit_card_expiry_date.should eq Time.local(2013, 9, 12)
     Faker::Business.credit_card_type.should eq "mastercard"
   end
 end
